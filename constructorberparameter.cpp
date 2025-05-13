cf9364b6e441662d9d939e24c1fc98295c16e05c
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-13 15:46:00 +0700

    Tahap 5

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -5,3 +5,10 @@ class mahasiswa{
 public:
     mahasiswa(int nim, string nama); 
 };
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama){
+    cout << "Constructor dengan parameter Terpanggil" << endl;
+    cout << "NIM   : " << nim << endl;
+    cout << "Nama  : " << nama << endl;
+}
